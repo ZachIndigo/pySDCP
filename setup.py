ef007c6631f33cfafd1f31a937d39c7831eb2200
zachir <zachir@librem.one>
2023-03-02 09:46:24 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     name='pySDCP',
     version='1',
     packages=['pysdcp'],
-    url='https://github.com/Galala7/pySDCP',
+    url='https://github.com/ZachIndigo/pySDCP',
     license='MIT',
-    author='Guy Shapira',
-    author_email='Galala7@users.noreply.github.com',
+    author='Zachary Smith',
+    author_email='mailto@zachir.xyz',
     description='SDCP library to control Sony Projectors',
     classifiers=[
         # How mature is this project? Common values are
@@ -26,7 +26,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.9',
     ],
     python_requires='>=3',
 )
